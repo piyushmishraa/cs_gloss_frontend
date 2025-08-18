31d80e946d39762a59737b30cd65138c73912b5c
Boss Piyush <fanboiofharrystyles@gmail.com>
2025-08-18 19:24:52 +0530

    handled api key

--- src/Pages/Article.jsx
+++ src/Pages/Article.jsx
@@ -3,11 +3,12 @@ import React, { useEffect, useState } from 'react'
 import ArticalCard from '../components/ArticalCard';
 
 import "./Article.css"
+const apiKey = import.meta.env.VITE_NEWS_API_KEY;
 
 const Article = () => {
    const[articles,setArticles]=useState([]);
    useEffect(()=>{
-    const articledata=axios.get('https://newsapi.org/v2/everything?q=computer+science+OR+programming+OR+AI+OR+machine+learning&sortBy=publishedAt&language=en&pageSize=10&apiKey=d7b65ef4aec942e7b80080c59513c07d');
+    const articledata=axios.get(`https://newsapi.org/v2/everything?q=computer+science+OR+programming+OR+AI+OR+machine+learning&sortBy=publishedAt&language=en&pageSize=10&apiKey=${apikey}`);
     articledata.then((response)=>{
         console.log(response.data.articles);
         setArticles(response.data.articles)
